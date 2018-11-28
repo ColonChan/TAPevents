d76d28e86f44875b2adb3283822d693fa7d3bdf6
Colon <colon1127@gmail.com>
2018-11-28 10:33:41 +0800

    update login page

--- coreui/src/views/Pages/Login/Login.js
+++ coreui/src/views/Pages/Login/Login.js
@@ -1,54 +1,66 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+import logoWhite from '../../../assets/img/brand/logowhite.svg'
+import mainbk from '../../../assets/img/mainbk.jpg'
 
 class Login extends Component {
   render() {
     return (
-      <div className="app flex-row align-items-center">
+      <div className="app flex-row align-items-center" style={{ position: 'relative' }}>
+        <span className="frontbk" />
         <Container>
           <Row className="justify-content-center">
             <Col md="8">
               <CardGroup>
-                <Card className="p-4">
-                  <CardBody>
-                    <Form>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
-                      <InputGroup className="mb-3">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-user"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="text" placeholder="Username" autoComplete="username" />
-                      </InputGroup>
-                      <InputGroup className="mb-4">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-lock"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="password" placeholder="Password" autoComplete="current-password" />
-                      </InputGroup>
-                      <Row>
-                        <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
-                        </Col>
-                        <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
-                        </Col>
-                      </Row>
-                    </Form>
+                <Card className="p-4 sibox">
+                  <CardBody className="login-form text-center">
+                    <h1>Admin Login</h1>
+                    <p className="text-muted mb-6">Sign In to manage your events</p>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Email" autoComplete="username" />
+                    </InputGroup>
+                    <div className="wraning-msg">
+                      {/* Warning message for LoginID */}
+                    </div>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Password" autoComplete="current-password" />
+                    </InputGroup>
+                    <div className="wraning-msg">
+                      {/* Warning message for Password */}
+                    </div>
+                    <Row>
+                      <Col xs="12" className="mb-6">
+                        <Button color="link" className="px-0 pt-0">Forgot password?</Button>
+                      </Col>
+                      <Col xs="12">
+                        <Button color="primary" className="px-4 btn-brand">Login</Button>
+                      </Col>
+                    </Row>
                   </CardBody>
                 </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Button color="primary" className="mt-3" active>Register Now!</Button>
-                    </div>
+                <Card className="text-white py-4 d-md-down-none sibar">
+                  <CardBody className="text-center p-3">
+                      <div className="inner-up">
+                        <span>Welcome to</span>
+                        <span><img src={logoWhite} className="svg-logowhite" alt="TAPevents" /></span>
+                        <span>Events Management BackOffice</span>
+                      </div>
+                      <div className="inner-down">
+                        <p>Have problems with login issues?</p>
+                        <Button color="primary" className="mt-3 btn-flat px-4">Contact Us</Button>
+                        {/* Suggest: button onclick mailto: something like HelpDesk */}
+                      </div>
                   </CardBody>
                 </Card>
               </CardGroup>
